51210b3490cc58aacb795e8989ea9c40850dab28
Jonatas Melo Silva <jonatas891024@gmail.com>
2022-12-13 14:44:16 +0000

    feat: adiciona resposta do exercico 2

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
-import models.Pessoa;
+import models.*;
+
 
 class Main {
     public static void main(String[] args) {
 
-        Pessoa pessoa = new Pessoa("Jonatas", 27);
-        System.out.println("Nome: " + pessoa.verNome());
-        System.out.println("Nome: " + pessoa.verIdade());
+        //Pessoa pessoa = new Pessoa("Jonatas", 27);
+        //System.out.println("Nome: " + pessoa.verNome());
+        //System.out.println("Nome: " + pessoa.verIdade());
+
+        Porta porta = new Porta("azul", 2.0f, 3.0f);
+
+        System.out.println("Cor: " + porta.verCor());
+        System.out.println("Está aberta: " + porta.estaAberta());
+
+        porta.abrir();
+        System.out.println("Está aberta: " + porta.estaAberta());
+        porta.fechar();
+        System.out.println("Está aberta: " + porta.estaAberta());
     }
 }
