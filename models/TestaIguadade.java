16e9c65f3e4601d11bbe5c47cbfa9f3e0667f89d
Jonatas Melo Silva <jonatas891024@gmail.com>
2022-12-20 13:28:24 +0000

    feat: adiciona teste de igualdade da questão 6

--- models/TestaIguadade.java
+++ models/TestaIguadade.java
@@ -6,9 +6,22 @@ public class TestaIguadade {
     Pessoa pessoa2 = new Pessoa("jonatas", 27);
 
     System.out.println(pessoa1 == pessoa2);
+    System.out.println(pessoa1);
+    System.out.println(pessoa2);
 
     Pessoa pessoa3 = pessoa1;
 
+    System.out.println(pessoa3);
+
     System.out.println(pessoa1.equals(pessoa3));
+    System.out.println(pessoa1.equals(pessoa2));
+
+    pessoa3 = pessoa2;
+
+    System.out.println(pessoa3);
+    System.out.println(pessoa2);
+    System.out.println(pessoa3 == pessoa2);
+
+    System.out.println(pessoa3.equals(pessoa1));
   }
 }
